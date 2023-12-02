a9515232fe500ba6bac3396998ecd0ca843a3ce0
Agustin <agustinteta@hotmail.com>
2023-12-01 21:36:38 -0300

    Fix app.js and add validation middleware

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -1,8 +1,15 @@
+const { validationResult } = require('express-validator');
+const { createProductValidation, updateProductValidation, deleteProductValidation } = require('../middleware/validationMiddleware');
 const Product = require('../models/productosModels');
 
 const getProduct = async (req, res) => {
     try {
         const products = await Product.find();
+
+        if (!products || products.length === 0) {
+            return res.status(404).send('No se encontraron productos');
+        }
+
         res.json(products);
     } catch (error) {
         console.error(error);
@@ -10,7 +17,15 @@ const getProduct = async (req, res) => {
     }
 };
 
+
 const createProduct = async (req, res) => {
+    await Promise.all(createProductValidation.map(validation => validation(req, res)));
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
     try {
         const newProduct = new Product(req.body);
         const savedProduct = await newProduct.save();
@@ -21,7 +36,15 @@ const createProduct = async (req, res) => {
     }
 };
 
+
 const updateProduct = async (req, res) => {
+    await Promise.all(updateProductValidation.map(validation => validation(req, res)));
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
     try {
         const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
         res.json(updatedProduct);
@@ -31,9 +54,23 @@ const updateProduct = async (req, res) => {
     }
 };
 
+
 const deleteProduct = async (req, res) => {
+    await Promise.all(deleteProductValidation.map(validation => validation(req, res)));
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
     try {
-        await Product.findByIdAndDelete(req.params.id);
+        const productToDelete = await Product.findById(req.params.id);
+
+        if (!productToDelete) {
+            return res.status(404).send('Producto no encontrado');
+        }
+
+        await productToDelete.deleteOne();
         res.send('Producto eliminado correctamente');
     } catch (error) {
         console.error(error);
@@ -41,9 +78,10 @@ const deleteProduct = async (req, res) => {
     }
 };
 
+
 module.exports = {
     getProduct,
-  createProduct,
-  updateProduct,
-  deleteProduct
+    createProduct,
+    updateProduct,
+    deleteProduct
 };
